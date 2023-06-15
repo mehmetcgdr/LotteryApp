cc86d60c36f665e95600a77371c3dc806331fd51
Mehmet ÖÇGÜDER <43685911+mehmetcgdr@users.noreply.github.com>
2023-06-15 22:58:09 +0300

    Add files via upload

---
+++ Loto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehmetOcguder_Odev_SayisalLoto
+{
+    
+   static internal class Loto
+    {
+              
+
+        static public int[] Tahmin()
+        {
+            
+            int[] tahmin = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                
+                Console.WriteLine($"\n{i+1}. Tahmininizi Giriniz: ");
+                tahmin[i]=Convert.ToInt32(Console.ReadLine());
+            }
+            return tahmin;
+        }
+       static public  int[] SayiUret()
+        {
+            Random  rnd = new Random(); 
+            int[] randomSayilar = new int[6];
+            for (int i = 0; i < 6; i++)
+            {
+                randomSayilar[i] = rnd.Next(1, 50);
+            }
+            Array.Sort(randomSayilar);
+            return randomSayilar;
+            
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace MehmetOcguder_Odev_SayisalLoto
+{
+
+
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayısal Loto Oyununa Hoşgeldiniz.");
+            Console.WriteLine("6 Adet tahmin hakkınız ile sayıları tahmin etmeye çalışacaksınız.");
+            
+            int dogru = 0;
+            bool kontrol = false;
+            
+
+            Console.WriteLine("Tahminlerinizi giriniz.");
+            Console.WriteLine("***********************************");
+            int[] randomSayilar = Loto.SayiUret();  //Hile kısmı
+            foreach (int i in randomSayilar)
+            {
+                
+                Console.Write($"{i}\t ");
+                Console.WriteLine("***HİLE***");
+            }
+            int[] girilenSayilar = Loto.Tahmin();
+            foreach (var random in randomSayilar)
+            {
+                foreach (var tahmin in girilenSayilar)
+                {
+
+                    if (random == tahmin)
+                    {
+                        Console.WriteLine($"Bildiğiniz Sayı : {random}");
+                        dogru += 1;
+                        kontrol = true;
+
+                    }
+                }
+            }
+            if (kontrol)
+            {
+                Console.WriteLine($"{dogru} tane bildiniz.");
+            }
+            else
+            {
+                Console.WriteLine("Malesef doğru tahminde bulunmadınız.");
+            }
+            foreach (var random in randomSayilar)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{random}");
+            }
+
+        }
+    }
+}
